73e80ee4bddfe3953fb18fb82fd41c3ca2e1cbda
Mark <miningmark48@gmail.com>
2020-04-09 01:46:24 -0400

    Backup skip if files are the same

--- util/backups.py
+++ util/backups.py
@@ -1,11 +1,17 @@
+import filecmp
 import os
 import shutil
 
 
 def backup_file(filename: str):
     folder_name = "backups"
+    backup_name = f"{folder_name}/{filename}"
     if not os.path.exists(folder_name):
         os.makedirs(folder_name)
 
-    shutil.copyfile(f"{filename}", f"{folder_name}/{filename}")
-    print(f"Backed up {filename} to {folder_name}/{filename}")
+    if filecmp.cmp(filename, backup_name):
+        print(f"{filename} is same as {backup_name}, skipping...")
+        return
+
+    shutil.copyfile(f"{filename}", backup_name)
+    print(f"Backed up {filename} to {backup_name}")
