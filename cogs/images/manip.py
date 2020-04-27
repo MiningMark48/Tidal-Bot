0d203b3a69c90e27d79f6a2969b53363ab8a22ac
Mark <miningmark48@gmail.com>
2020-04-27 00:21:46 -0400

    Adding image manipulation
    - Pixelate

--- cogs/images/manip.py
+++ cogs/images/manip.py
@@ -10,6 +10,37 @@ class Images(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command(name="imblur", aliases=["imb"])
+    @commands.cooldown(1, 3, commands.BucketType.user)
+    async def blur(self, ctx, url=None, radius=5):
+        """
+        Image Manipulation: Blur
+        """
+
+        if not url:
+            url = ctx.author.avatar_url
+
+        try:
+            r = requests.get(url, timeout=2)
+            buffer = BytesIO(r.content)
+
+            async with ctx.typing():
+                with Image.open(buffer) as im:
+                    im = im.convert("RGB")
+
+                    im = im.filter(ImageFilter.GaussianBlur(radius=radius))
+
+                    final_buffer = BytesIO()
+                    im.save(final_buffer, "png")
+
+                final_buffer.seek(0)
+                file = discord.File(filename=f"manipulated_image.png", fp=final_buffer)
+                await ctx.send(file=file)
+
+        except UnidentifiedImageError:
+            await ctx.send("Invalid URL!")
+            return
+
     @commands.command(name="imgrayscale", aliases=["imgs"])
     @commands.cooldown(1, 3, commands.BucketType.user)
     async def grayscale(self, ctx, url=None):
@@ -27,6 +58,39 @@ class Images(commands.Cog):
             async with ctx.typing():
                 with Image.open(buffer) as im:
                     im = im.convert("LA")
+
+                    final_buffer = BytesIO()
+                    im.save(final_buffer, "png")
+
+                final_buffer.seek(0)
+                file = discord.File(filename=f"manipulated_image.png", fp=final_buffer)
+                await ctx.send(file=file)
+
+        except UnidentifiedImageError:
+            await ctx.send("Invalid URL!")
+            return
+
+    @commands.command(name="impixelate", aliases=["imp"])
+    @commands.cooldown(1, 3, commands.BucketType.user)
+    async def pixelate(self, ctx, url=None):
+        """
+        Image Manipulation: Pixelate
+        """
+
+        if not url:
+            url = ctx.author.avatar_url
+
+        try:
+            r = requests.get(url, timeout=2)
+            buffer = BytesIO(r.content)
+
+            async with ctx.typing():
+                with Image.open(buffer) as im:
+                    im = im.convert("RGB")
+                    og_size = im.size
+
+                    im = im.resize((32, 32), resample=Image.BILINEAR)
+                    im = im.resize(og_size, Image.NEAREST)
 
                     final_buffer = BytesIO()
                     im.save(final_buffer, "png")
@@ -69,37 +133,6 @@ class Images(commands.Cog):
                             tb = min(int(0.272 * r + 0.534 * g + 0.131 * b), 255)
 
                             pixels[px, py] = (tr, tg, tb)
-
-                    final_buffer = BytesIO()
-                    im.save(final_buffer, "png")
-
-                final_buffer.seek(0)
-                file = discord.File(filename=f"manipulated_image.png", fp=final_buffer)
-                await ctx.send(file=file)
-
-        except UnidentifiedImageError:
-            await ctx.send("Invalid URL!")
-            return
-
-    @commands.command(name="imblur", aliases=["imb"])
-    @commands.cooldown(1, 3, commands.BucketType.user)
-    async def blur(self, ctx, url=None, radius=5):
-        """
-        Image Manipulation: Blur
-        """
-
-        if not url:
-            url = ctx.author.avatar_url
-
-        try:
-            r = requests.get(url, timeout=2)
-            buffer = BytesIO(r.content)
-
-            async with ctx.typing():
-                with Image.open(buffer) as im:
-                    im = im.convert("RGB")
-
-                    im = im.filter(ImageFilter.GaussianBlur(radius=radius))
 
                     final_buffer = BytesIO()
                     im.save(final_buffer, "png")
