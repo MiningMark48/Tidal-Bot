a9eff81d1cd5ea46faaf3a2153fb4417ae6ed557
Mark <miningmark48@gmail.com>
2020-06-02 14:11:31 -0400

    This didn't need to be done

--- cogs/servmng/servmng.py
+++ cogs/servmng/servmng.py
@@ -38,13 +38,7 @@ class ServerManagement(commands.Cog, name="Server Management"):
 
     @commands.group(name="joinmessage", aliases=["joinmsg"])
     async def join_msg(self, ctx):
-        """
-        Join message is a message to be sent to a user when they join the server.
-
-        Commands: Set, Get, Clear
-
-        Usage: joinmessage get
-        """
+        """Join message is a message to be sent to a user when they join the server."""
         pass
 
     @join_msg.command(name="set")
