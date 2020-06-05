8756b217a8969bcb06c6917e08b8f187f1bc7933
Mark <miningmark48@gmail.com>
2020-06-05 14:15:28 -0400

    Compressing backups into zip now

--- util/data/data_backup.py
+++ util/data/data_backup.py
@@ -1,6 +1,5 @@
-import filecmp
 import os
-import shutil
+import zipfile
 from datetime import datetime as dt
 from os import listdir
 from os import path
@@ -9,29 +8,42 @@ from os.path import isfile, join
 from util.logger import Logger
 
 data_path = "data/"
+backups_folder_name = "backups"
+zip_name = "backups.zip"
 
 
-def backup_databases():
+def backup_databases(always_run=True):
+    subfolder_name = get_subfolder_name()
+
+    if path.exists(f"{backups_folder_name}/{subfolder_name}/{zip_name}") and not always_run:
+        return
+
+    loczip = f"{backups_folder_name}/{subfolder_name}/{zip_name}"
+    zip = zipfile.ZipFile(loczip, "w", zipfile.ZIP_DEFLATED)
+
     only_files = [f for f in listdir(data_path) if isfile(join(data_path, f))]
     for file in only_files:
-        backup_file(file)
+        backup_file(zip, f"{data_path}{file}")
+
+    Logger.info(f"Backed up {len(only_files)} files to {backups_folder_name}/{subfolder_name}/{zip_name}")
 
 
-def backup_file(filename: str):
-    date = dt.now()
-    folder_name = "backups"
-    subfolder_name = f"{date.strftime('%m%d%y')}"
-    backup_name = f"{folder_name}/{subfolder_name}/{filename}"
+def get_subfolder_name():
+    return str(dt.now().strftime('%m%d%y'))
+
+
+def backup_file(zip, filename: str):
+
+    folder_name = backups_folder_name
+    subfolder_name = get_subfolder_name()
+    # backup_loc = f"{folder_name}/{subfolder_name}/"
+    # backup_name = f"{backup_loc}/{filename}"
     if not os.path.exists(folder_name):
         os.makedirs(folder_name)
 
     if not os.path.exists(f"{folder_name}/{subfolder_name}"):
         os.makedirs(f"{folder_name}/{subfolder_name}")
 
-    if path.exists(f"{data_path}{filename}") and path.exists(backup_name):
-        if filecmp.cmp(f"{data_path}{filename}", backup_name):
-            # print(f"{filename} is same as {backup_name}, skipping...")
-            return
+    zip.write(filename, os.path.basename(filename))
 
-    shutil.copyfile(f"{data_path}{filename}", backup_name)
-    Logger.info(f"Backed up {data_path}{filename} to {backup_name}")
+    # Logger.info(f"Backed up {filename} to {backup_loc}{zip_name}")
