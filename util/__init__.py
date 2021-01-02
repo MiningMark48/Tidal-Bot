0fbf54aabc643d39d5b68a40c4e156da400cfdda
Mark <miningmark48@gmail.com>
2021-01-02 02:03:10 -0500

    init py tweaks

--- util/__init__.py
+++ util/__init__.py
@@ -1,5 +1,6 @@
+import data
 from .config import BotConfig
-from.extensions import get_extensions
+from .extensions import get_extensions
 from .decorators import delete_original
 from .checks import is_bot_owner
 from .spongemock import mockify
---
+++ util/data/__init__.py
@@ -0,0 +1,2 @@
+from .table_helper import TableHelper
+from .value_helper import ValueHelper
